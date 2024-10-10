5319e206db8fd95b633ae66edc1effe6782dff63
handyman023 <134408989+handyman023@users.noreply.github.com>
2024-10-11 00:55:26 +0200

    First Commit
    11/10/2025

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Collect form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').
